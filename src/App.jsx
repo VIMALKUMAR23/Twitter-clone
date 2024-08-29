0f20ae62278b2899bd263472f39ac54677a71edf
Vimal <vimal01547@gmail.com>
2024-08-29 10:45:31 +0530

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      my x-twitter app
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md justify-center space-y-4 mt-5 i">
+        <h1 className="text-3xl font-bold underline bgre">building x-twitter clone!</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
